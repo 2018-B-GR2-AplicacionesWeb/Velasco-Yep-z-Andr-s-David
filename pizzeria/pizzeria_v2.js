d0ee10bc6500709f86aa582c1cad7a76ca765df8
Andrés Velasco <velasco.andrs@gmail.com>
2018-12-03 23:11:02 -0500

    examen y proyecto

---
+++ pizzeria/pizzeria_v2.js
@@ -0,0 +1,364 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const rxjs = require('rxjs');
+const timer = require('rxjs').timer;
+const mergeMap = require('rxjs/operators').mergeMap;
+const map = require('rxjs/operators').map;
+const retryWhen = require('rxjs/operators').retryWhen;
+const delayWhen = require('rxjs/operators').delayWhen;
+//const preguntaMenu_Inicio = require('./preguntas').preguntaMenu_Inicio;
+const preguntaMenu_Inicio = {
+    type: 'list',
+    name: 'opcionMenu',
+    message: 'Login',
+    choices: [
+        'Modulo Pizzas',
+        'Modulo Clientes',
+    ]
+};
+const preguntaEdicion = [
+    {
+        type: 'input',
+        name: 'nombre',
+        message: 'Cual es el nuevo nombre?'
+    },
+];
+const preguntaMenu_crud = {
+    type: 'list',
+    name: 'opcionMenu',
+    message: 'Que quieres hacer',
+    choices: [
+        'Buscar',
+        'Crear',
+        'Actualizar',
+        'Borrar',
+    ]
+};
+const pregunta_ingresar_pizza = [
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Ingrese ID Pizza',
+    },
+    {
+        type: 'input',
+        name: 'nombre',
+        message: 'Nombre de pizza',
+    }
+];
+const pregunta_ingresar_cliente = [
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Ingrese ID Cliente',
+    },
+    {
+        type: 'input',
+        name: 'nombre',
+        message: 'Nombre del Cliente',
+    }
+];
+const preguntaBuscar_pizza = [
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Ingrese ID Pizza',
+    }
+];
+const preguntaBuscar_cliente = [
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Ingrese ID Cliente',
+    }
+];
+/*
+*/
+function incializarBDD() {
+    // @ts-ignore
+    return new Promise((resolve, reject) => {
+        fs.readFile('bdd.json', 'utf-8', (error, contenidoArchivo) => {
+            if (error) {
+                fs.writeFile('bdd.json', '{"clientes":[],"pizzas":[]}', (error) => {
+                    if (error) {
+                        reject({
+                            mensaje: 'Error creando',
+                            error: 500
+                        });
+                    }
+                    else {
+                        resolve({
+                            mensaje: 'BDD leida',
+                            bdd: JSON.parse('{"clientes":[],"pizzas":[]}')
+                        });
+                    }
+                });
+            }
+            else {
+                resolve({
+                    mensaje: 'BDD leida',
+                    bdd: JSON.parse(contenidoArchivo)
+                });
+            }
+        });
+    });
+}
+// @ts-ignore
+async function main() {
+    //1) Inicializar bdd
+    //2) Preguntas Login
+    //2.1) Admin
+    //2.2) Cliente
+    //3) Opciones de Respuespuesta
+    //4) ACCION
+    //5)GUARDAR BDD
+    const respuestaBDD$ = rxjs.from(incializarBDD());
+    respuestaBDD$
+        .pipe(preguntarOpcionMenu_Inicio(), opcionesRespuesta_inicio())
+        .subscribe(() => { }, (error) => { console.log(error); }, () => { main(); console.log("Completado"); });
+}
+function preguntarOpcionMenu_Inicio() {
+    //
+    return mergeMap(// Respuesta Anterior Observable
+    (respuestaBDD) => {
+        return rxjs
+            .from(inquirer.prompt(preguntaMenu_Inicio))
+            .pipe(map(// respuesta ant obs
+        (respuesta) => {
+            respuestaBDD.opcioMenu_inicio = respuesta;
+            return respuestaBDD;
+        }));
+    });
+}
+function opcionesRespuesta_inicio() {
+    return mergeMap((respuestaBDD) => {
+        const opcion = respuestaBDD.opcioMenu_inicio.opcionMenu;
+        switch (opcion) {
+            case 'Modulo Pizzas':
+                return rxjs
+                    .from(incializarBDD())
+                    .pipe(preguntarOpciones_crud(), opcionesRespuesta_crud(), ejecutarAccion_crud(), guardarBaseDeDatos());
+            case 'Modulo Clientes':
+                return rxjs
+                    .from(incializarBDD())
+                    .pipe(preguntarOpciones_crud(), opcionesRespuesta_crud('cliente'), ejecutarAccion_crud('cliente'), guardarBaseDeDatos());
+        }
+    });
+}
+function preguntarOpciones_crud() {
+    return mergeMap(// Respuesta Anterior Observable
+    (respuestaBDD) => {
+        return rxjs
+            .from(inquirer.prompt(preguntaMenu_crud))
+            .pipe(map(// respuesta ant obs
+        (respuesta) => {
+            respuestaBDD.opcionMenu_pizzas = respuesta;
+            console.log(respuestaBDD.opcionMenu_pizzas);
+            return respuestaBDD;
+        }));
+    });
+}
+function opcionesRespuesta_crud(entidad = 'pizza') {
+    return mergeMap((respuestaBDD) => {
+        const opcion = respuestaBDD.opcionMenu_pizzas.opcionMenu;
+        console.log(respuestaBDD.opcionMenu_pizzas.opcionMenu);
+        switch (opcion) {
+            case 'Crear':
+                if (entidad === 'pizza') {
+                    return rxjs
+                        .from(inquirer.prompt(pregunta_ingresar_pizza))
+                        .pipe(map((pizza) => {
+                        respuestaBDD.pizza = pizza;
+                        return respuestaBDD;
+                    }));
+                }
+                else {
+                    return rxjs
+                        .from(inquirer.prompt(pregunta_ingresar_cliente))
+                        .pipe(map((cliente) => {
+                        respuestaBDD.cliente = cliente;
+                        return respuestaBDD;
+                    }));
+                }
+            case 'Buscar':
+                if (entidad === 'pizza') {
+                    return consultar_pizza_por_id(respuestaBDD);
+                }
+                else {
+                    return consultar_cliente_por_id(respuestaBDD);
+                }
+            case 'Actualizar':
+                if (entidad === 'pizza') {
+                    return consultar_pizza_por_id(respuestaBDD, true);
+                }
+                else {
+                    return consultar_cliente_por_id(respuestaBDD, true);
+                }
+            case 'Borrar':
+                if (entidad === 'pizza') {
+                    return consultar_pizza_por_id(respuestaBDD);
+                }
+                else {
+                    return consultar_cliente_por_id(respuestaBDD);
+                }
+        }
+    });
+}
+function actualizar_entidad(respuestaBDD, entidad = 'pizza') {
+    return rxjs
+        .from(inquirer.prompt(preguntaEdicion))
+        .pipe(mergeMap((respuesta) => {
+        if (entidad === 'pizza') {
+            respuestaBDD.pizza = {
+                id: null,
+                nombre: respuesta.nombre,
+            };
+        }
+        else {
+            respuestaBDD.cliente = {
+                id: null,
+                nombre: respuesta.nombre,
+            };
+        }
+        return rxjs
+            .from(incializarBDD())
+            .pipe(map(() => {
+            return respuestaBDD;
+        }));
+    }));
+}
+function consultar_pizza_por_id(respuestaBDD, actualizar = false) {
+    return rxjs
+        .from(inquirer.prompt(preguntaBuscar_pizza))
+        .pipe(mergeMap((respuesta) => {
+        //const indice = pizza.id;
+        const indicePizza = respuestaBDD.bdd
+            .pizzas.findIndex((pizza) => {
+            return pizza.id === respuesta.id;
+        });
+        if (indicePizza === -1) {
+            console.log('No existe ese id por favor ingrese otro: ');
+            return consultar_pizza_por_id(respuestaBDD);
+        }
+        else {
+            respuestaBDD.indicePizza = indicePizza;
+            //respuestaBDD.pizza = respuestaBDD.bdd.pizzas[indicePizza].nombre;
+            //return respuestaBDD;
+            if (actualizar) {
+                return actualizar_entidad(respuestaBDD);
+            }
+            else {
+                return rxjs
+                    .from(incializarBDD())
+                    .pipe(map(() => {
+                    return respuestaBDD;
+                }));
+            }
+        }
+    }));
+}
+function consultar_cliente_por_id(respuestaBDD, actualizar = false) {
+    return rxjs
+        .from(inquirer.prompt(preguntaBuscar_cliente))
+        .pipe(mergeMap((respuesta) => {
+        //const indice = pizza.id;
+        const indiceCliente = respuestaBDD.bdd
+            .clientes.findIndex((cliente) => {
+            return cliente.id === respuesta.id;
+        });
+        if (indiceCliente === -1) {
+            console.log('No existe ese id por favor ingrese otro: ');
+            return consultar_pizza_por_id(respuestaBDD);
+        }
+        else {
+            respuestaBDD.indiceCliente = indiceCliente;
+            //respuestaBDD.pizza = respuestaBDD.bdd.pizzas[indiceCliente].nombre;
+            //return respuestaBDD;
+            if (actualizar) {
+                return actualizar_entidad(respuestaBDD, 'cliente');
+            }
+            else {
+                return rxjs
+                    .from(incializarBDD())
+                    .pipe(map(() => {
+                    return respuestaBDD;
+                }));
+            }
+        }
+    }));
+}
+function ejecutarAccion_crud(entidad = 'pizza') {
+    return map(// Respuesta del anterior OBS
+    (respuestaBDD) => {
+        const opcion = respuestaBDD.opcionMenu_pizzas.opcionMenu;
+        switch (opcion) {
+            case 'Crear':
+                if (entidad === 'pizza') {
+                    const pizza = respuestaBDD.pizza;
+                    respuestaBDD.bdd.pizzas.push(pizza);
+                }
+                else {
+                    const cliente = respuestaBDD.cliente;
+                    respuestaBDD.bdd.clientes.push(cliente);
+                }
+                return respuestaBDD;
+            case 'Actualizar':
+                if (entidad === 'pizza') {
+                    const indice = respuestaBDD.indicePizza;
+                    respuestaBDD.bdd.pizzas[indice].nombre = respuestaBDD.pizza.nombre;
+                }
+                else {
+                    const indice = respuestaBDD.indiceCliente;
+                    respuestaBDD.bdd.clientes[indice].nombre = respuestaBDD.cliente.nombre;
+                }
+                return respuestaBDD;
+            case 'Buscar':
+                if (entidad === 'pizza') {
+                    console.log(`Pizza encontrada: ${respuestaBDD.bdd.pizzas[respuestaBDD.indicePizza].nombre}`);
+                }
+                else {
+                    console.log(`Cliente encontrado: ${respuestaBDD.bdd.clientes[respuestaBDD.indiceCliente].nombre}`);
+                }
+                return respuestaBDD;
+            case 'Borrar':
+                if (entidad === 'pizza') {
+                    console.log(`Pizza a borrar: ${respuestaBDD.bdd.pizzas[respuestaBDD.indicePizza].nombre}`);
+                    const indice_borrar = respuestaBDD.indicePizza;
+                    respuestaBDD.bdd.pizzas.splice(indice_borrar, 1);
+                }
+                else {
+                    console.log(`Cliente a borrar: ${respuestaBDD.bdd.clientes[respuestaBDD.indiceCliente].nombre}`);
+                    const indice_borrar = respuestaBDD.indiceCliente;
+                    respuestaBDD.bdd.clientes.splice(indice_borrar, 1);
+                }
+                return respuestaBDD;
+        }
+    });
+}
+function guardarBaseDeDatos() {
+    return mergeMap(// Respuesta del anterior OBS
+    (respuestaBDD) => {
+        // OBS
+        return rxjs.from(guardarBDD(respuestaBDD.bdd));
+    });
+}
+function guardarBDD(bdd) {
+    // @ts-ignore
+    return new Promise((resolve, reject) => {
+        fs.writeFile('bdd.json', JSON.stringify(bdd), (error) => {
+            if (error) {
+                reject({
+                    mensaje: 'Error creando',
+                    error: 500
+                });
+            }
+            else {
+                resolve({
+                    mensaje: 'BDD guardada',
+                    bdd: bdd
+                });
+            }
+        });
+    });
+}
+main();
